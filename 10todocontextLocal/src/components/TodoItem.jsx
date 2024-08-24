396276bd9e28893cd128526f1a237ab4e6dd223f
hema <bhemavallika@gmail.com>
2024-08-24 18:45:55 +0530

    updated todoitem

--- 10todocontextLocal/src/components/TodoItem.jsx
+++ 10todocontextLocal/src/components/TodoItem.jsx
@@ -4,16 +4,17 @@ import { useTodo } from '../contexts';
 function TodoItem({ todo }) {
     const [isTodoEditable,setIsTodoEditable] = useState(false)
     const [todoMsg,setTodoMsg] = useState(todo.todo)
-    const {updateTode,deleteTodo,toggleComplete} = useTodo()
+    const {updateTodo,deleteTodo,toggleComplete} = useTodo()
 
     const editTodo = () =>{
-        updateTode(todo.id,{...todo, todo: todoMsg})
+        updateTodo(todo.id,{...todo, todo: todoMsg})
         setIsTodoEditable(false)
     }
 
     const toggleCompleted = () => {
         toggleComplete(todo.id)
     }
+
     return (
         <div
             className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -33,17 +34,21 @@ function TodoItem({ todo }) {
                 } ${todo.completed ? "line-through" : ""}`}
                 value={todoMsg}
                 onChange={(e) => setTodoMsg(e.target.value)}
-                readOnly={!isTodoEditable}
+                // readOnly={!isTodoEditable}
             />
             {/* Edit, Save Button */}
             <button
                 className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0 disabled:opacity-50"
                 onClick={() => {
+                    // console.log(isTodoEditable);
+                    
                     if (todo.completed) return;
 
                     if (isTodoEditable) {
                         editTodo();
-                    } else setIsTodoEditable((prev) => !prev);
+                    } else {
+                        setIsTodoEditable((prev) => !prev);
+                    }
                 }}
                 disabled={todo.completed}
             >
